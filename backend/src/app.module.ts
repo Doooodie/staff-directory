7a72f1f48ca973b20326456c79d886050d3465a1
Doooodie <unfair.slenderman@gmail.com>
2026-07-24 17:34:48 +0200

    feat: generate department entity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DepartmentModule } from './department/department.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: false,
       }),
     }),
+    DepartmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
