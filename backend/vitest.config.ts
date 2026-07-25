1a511887f1ed10961610d63768142cbb578a3c82
Doooodie <unfair.slenderman@gmail.com>
2026-07-25 18:59:52 +0200

    fix: vitest oxc warning

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,7 @@ import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  oxc: false,
   test: { globals: true },
   plugins: [swc.vite({ module: { type: 'es6' } })],
 });
