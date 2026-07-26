4f2a0fb3cafc19c367e79400063bea1c6cc7b3f4
Doooodie <unfair.slenderman@gmail.com>
2026-07-26 16:52:48 +0200

    fix: tests mocking

--- backend/src/department/department.service.spec.ts
+++ backend/src/department/department.service.spec.ts
@@ -1,8 +1,12 @@
+import { createMock } from '@golevelup/ts-vitest';
 import { Test } from '@nestjs/testing';
 
 import { DepartmentService } from './department.service';
 
 import type { TestingModule } from '@nestjs/testing';
+import type { Repository } from 'typeorm';
+
+import type { Department } from './entities/department.entity';
 
 describe('DepartmentService', () => {
   let service: DepartmentService;
@@ -10,7 +14,9 @@ describe('DepartmentService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [DepartmentService],
-    }).compile();
+    })
+      .useMocker(() => createMock<Repository<Department>>())
+      .compile();
 
     service = module.get<DepartmentService>(DepartmentService);
   });
