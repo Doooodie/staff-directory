5e381cad2c906f730dc280e2c9569068cda4cc07
Doooodie <unfair.slenderman@gmail.com>
2026-07-31 02:52:02 +0200

    fix: vitest doesn't recognize path aliases

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 
@@ -5,4 +7,9 @@ export default defineConfig({
   oxc: false,
   test: { globals: true },
   plugins: [swc.vite({ module: { type: 'es6' } })],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
 });
