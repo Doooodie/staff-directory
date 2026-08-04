a828d5479ea8c1bbfe95591c812dea894cf2e9cb
Doooodie <unfair.slenderman@gmail.com>
2026-08-04 18:16:24 +0200

    fix: auth/login api contract

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -29,11 +29,11 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    const payload = { sub: user.id, username: user.email, role: user.role };
+    const payload = { sub: user.id, email: user.email, role: user.role };
 
     return {
-      access_token: await this.jwtService.signAsync(payload),
-      user,
+      accessToken: await this.jwtService.signAsync(payload),
+      user: { id: user.id, email: user.email, role: user.role },
     };
   }
 
